7788adbb74387dd88990b7543ba24f42ab5699e1
Farid Hernandez <farid.hdz.64@gmail.com>
2020-08-19 19:13:14 -0600

    Updates

---
+++ precache-manifest.00897819a30eaa41cdb83f81a20ac716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3713b1dd7c0dd547d343a6f300ca6dfd",
+    "url": "/fepade1/index.html"
+  },
+  {
+    "revision": "3ccc153bfc36ca324d58",
+    "url": "/fepade1/static/css/main.2fe6119e.chunk.css"
+  },
+  {
+    "revision": "92ecc1f195dc3454188c",
+    "url": "/fepade1/static/js/2.ceea7bff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fepade1/static/js/2.ceea7bff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ccc153bfc36ca324d58",
+    "url": "/fepade1/static/js/main.df9cc9a8.chunk.js"
+  },
+  {
+    "revision": "0f8a8f37b5e0aefc6f46",
+    "url": "/fepade1/static/js/runtime-main.53913590.js"
+  }
+]);
